1f16d276cf692cd6c792789061f26b5882da141f
Alaa Diab <57873585+AlaaDiab7@users.noreply.github.com>
2024-12-31 02:36:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ function openCertificate(fileName) {
     const url = `certificates/${fileName}`;
     window.open(url, '_blank');
 }
+
+document.querySelectorAll('a').forEach(link => {
+    link.addEventListener('mouseover', () => {
+        link.style.color = '#ff5722';
+    });
+    link.addEventListener('mouseout', () => {
+        link.style.color = '';
+    });
+});
